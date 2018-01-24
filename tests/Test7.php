cc91c583a269008fa33c55a9a3b562ffd24800c1
g-gar <g.gar.fern@hotmail.com>
2018-01-24 21:46:23 +0100

    20180124

--- tests/Test7.php
+++ tests/Test7.php
@@ -25,26 +25,81 @@ class BookList {
 	private $books;
 
 	function __construct(){
-		$this->books = array()
+		$this->books = array();
 	}
 	function getBookCount() {
-		return count($this->books());
+		return count($this->books);
 	}
 	function getBook($index){
 		return is_numeric($index) && $index < $this->getBookCount() ? $this->books[$index] : null;
 	}
 	function addBook(Book $book) {
-		$this->books[] = $book;
+		$this->books[$this->getBookCount()] = $book;
 	}
 	function removeBook(Book $book) {
 		if ($i = array_search($book, $books)) {
-			array_splice($books, $i);
+			array_splice($books, $i, 1);
 		}
 	}
 }
 
-class main {
-	function __constructor(){
+class BookListIterator {
+	protected $bookList;
+	protected $current;
 
+	function __construct(BookList $bookList){
+		$this->bookList = $bookList;
+		$this->current = 0;
+	}
+
+	function getCurrentBook(){
+		return $this->current < $this->bookList->getBookCount() ? $this->bookList->getBook($this->current) : null;
+	}
+	function getNextBook(){
+		return $this->hasNextBook() ? $this->bookList->getBook($this->current++) : null;
+	}
+	function hasNextBook(){
+		return $this->current < $this->bookList->getBookCount() -1 ? true : false;
+	}
+}
+
+class main {
+	function __construct(){
+
+		ini_set("display_errors", 1);
+		error_reporting(E_ALL);
+
+		function writeln($line_in) {
+			echo $line_in."<br/>";
+		}
+		writeln('BEGIN TESTING ITERATOR PATTERN');
+		writeln('');
+
+		$firstBook = new Book('Core PHP Programming, Third Edition', 'Atkinson and Suraski');
+		$secondBook = new Book('PHP Bible', 'Converse and Park');
+		$thirdBook = new Book('Design Patterns', 'Gamma, Helm, Johnson, and Vlissides');
+
+		$books = new BookList();
+		$books->addBook($firstBook);
+		$books->addBook($secondBook);
+		$books->addBook($thirdBook);
+
+		writeln('Testing the Iterator');
+
+		$booksIterator = new BookListIterator($books);
+
+		while ($booksIterator->hasNextBook()) {
+			$book = $booksIterator->getNextBook();
+			writeln('getting next book with iterator :');
+			writeln($book->getAuthorAndTitle());
+			writeln('');
+		}
+
+		$book = $booksIterator->getCurrentBook();
+		writeln('getting current book with iterator :');
+		writeln($book->getAuthorAndTitle());
+		writeln('');  
+
+		writeln('END TESTING ITERATOR PATTERN');
 	}
 }
