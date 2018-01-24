2c9653ec77ae9c0f7688ad1e5f2f2e8258691d2b
g-gar <gabriel.garcia@intalsis.com>
2018-01-24 11:18:22 +0100

    20180124

--- app/test.php
+++ app/test.php
@@ -1,9 +1,23 @@
 <?php
 
-	if (count($args[0]) > 0) {
-		require "tests/$args[0].php";
-		$test_ns = "\\Tests\\$args[0]";
-		$test = "$test_ns\\main";
+	ini_set("display_errors", 1);
 
-		$test = new $test();
+	$path_tests = 'tests/';
+	$avaiable_tests = array_map(function($element){
+		return trim($element, '.php');
+	}, array_diff(scandir($path_tests), array('..', '.')));
+
+	if (count($args[0]) > 0) {
+		try {
+			if (($t = array_search($args[0], $avaiable_tests)) >= 0) {
+				//TODO: handle this error
+				require $path_tests . $avaiable_tests[$t] . '.php';
+				$test_ns = "\\Tests\\$args[0]";
+				$test = "$test_ns\\main";
+
+				$test = new $test();
+			}
+		} catch (\Exception $e) {
+			echo "Test invalid";
+		}
 	} else echo "No test found";
