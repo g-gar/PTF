1596f9fef8e3bb7d8c4906bfc7d6629842d71af4
g-gar <g.gar.fern@hotmail.com>
2018-09-21 10:32:41 +0200

    Update
    simplified

--- lib/Router.php
+++ lib/Router.php
@@ -1,24 +1,8 @@
 <?php
-
-function is_uppercase($letter) {
-	return in_array(ord($letter), range(65, 90));
-}
-function is_lowercase($letter) {
-	return in_array(ord($letter), range(97, 122));
-}
-
 spl_autoload_register(function($classname){
 	$temp = preg_match("/TestFramework.*/", $classname) ? "lib" : "classes";
 	$temp = $temp . "/" . str_replace("\\", "/", $classname) .".php";
 	$name = array_reverse(explode("\\", $classname))[0];
-
-/*	if ( is_uppercase($name[0]) && is_lowercase($name[1]) ) { // is class
-
-	} else if ( is_uppercase($name[0]) && is_uppercase($name[1]) && $name[0] == 'I' ) { // is interface
-		//$temp = str_replace("/I", "/", $temp);
-	} else if ( is_uppercase($name[0]) && is_uppercase($name[1]) && $name[0] == 'E' ) { // is enum
-		//$temp = str_replace("/E", "/", $temp);
-	} else Throw new \Exception("Required element not supported");*/
 
 	require $temp;
 });
